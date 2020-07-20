cbc98eafc640556470866d4c610327128e034aee
lychenus <53417646+lychenus@users.noreply.github.com>
2020-07-20 10:05:46 +0800

    Update readStanfordSleepStage.py
    finish july20 2020

--- readStanfordSleepStage.py
+++ readStanfordSleepStage.py
@@ -1,67 +1,66 @@
-import sys
-import os
-
 folderpath="C:\\PSG\\0 standford - edit\\docunmentation\\"
-filename="Sleep profile.txt"
+filename="Flow Events.txt"
 filepath=folderpath+filename
 
 with open(filepath) as fp:
     line=fp.read().splitlines() #splitlines removes \n end of the line
     #print("the whole file of txt is")
+    #print(line[1][-8:]) #second line initial time
+    initialTime = line[1][-8:].split(":")  # first element in first splitted data is the initial time,
+    #print(initialTime)
+    initialTime = int(initialTime[2]) + int(initialTime[1]) * 60 + int(initialTime[0]) * 60 * 60
+    #print(initialTime)
 
-    del line[0:7]  #only in this file, others has to be reassessed
+    del line[0:5]  #only in this file, others has to be reassessed
     #print(line)
     splitted = []
-    montageCount=0
 
     # split string into time and stage
     for element in line:
         #print(element)
-        splitted.append(element.split("; "))
+        splitted.append(element.replace("-",";").split(";"))
+        #split line into [start, end, duration, type], since the start-end is seperated with -, replace - with ;, then split
         #print(element.split("; "))
+    #print(splitted)
 
     # change times into start time in seconds
     for element in splitted:
-        element[0]=montageCount*30
-        montageCount = montageCount + 1
-        #cut time minus
-        #print(element[0])
+        # split time into HH,MM,SS
+        element[0] = element[0][0:8].split(":")
+        element[1] = element[1][0:8].split(":")
+        element[2] = int(element[2])
+        # make time into 24-36h format, with 1am=25:00 for calculation purposes
+        if int(element[0][0]) < 13:
+            element[0][0] = int(element[0][0]) + 24
+        if int(element[1][0]) < 13:
+            element[1][0] = int(element[1][0]) + 24
+
+        # covert into seconds
+        element[0] = int(element[0][2]) + int(element[0][1]) * 60 + int(element[0][0]) * 60 * 60
+        element[1] = int(element[1][2]) + int(element[1][1]) * 60 + int(element[1][0]) * 60 * 60
+
+    # previous loop is looping elements in splitted
+
+    #linear transform against initialTime, for both start and the end time
+    for element in splitted:
+        element[0] = element[0] - initialTime
+        element[1] = element[1] - initialTime
+
     #print(splitted)
 
-    splitted2 = []
-    splitted2.append(splitted[0]) #put first element into splitted2
-    n=1
-    #print(len(splitted))
-
-    #if the sleep stage is the same as the previous, then do not append the stage, otherwise do.
-    while n<len(splitted):
-        if splitted[n][1]!=splitted2[-1][1]:
-            splitted2.append(splitted[n])
-        n=n+1
-    #result is that we have a list of time when the sleep phase changes
-
-    #change sleep stages into sleepwareG3 compilable names
-    n=0
-    while n<len(splitted2):
-        if splitted2[n][1]=='N1':
-            splitted2[n][1]="NonREM1"
-        if splitted2[n][1]=='N2':
-            splitted2[n][1]="NonREM2"
-        if splitted[ n][1]=='N3':
-            splitted[n][1]="NonREM3"
-        n=n+1
-    #print(splitted)
-    #print(splitted2)
-
-    #generate <XML> tags for the sleep stage
-    # <Stage Type="NonREM1" Start="24330" />
     XMLlist=[]
-    for record in splitted2:
-        XMLlist.append("<Stage Type=\""+str(record[1])+"\" Start=\""+str(record[0])+"\" />")
-
+    #result <Event Family="Respiratory" Type="ObstructiveApnea" Start="37.5" Duration="6.5" Machine="true">
+    #need to add all the inside useless tags, may assign number as 0
+    for element in splitted:
+        XMLlist.append("<Event Family=\"Respiratory\" Type=\""+ str(element[3]) + "\" Start=\""+ str(element[0]) +"\" Duration=\""+ str(element[2])+"\" Machine=\"true\">")
+        XMLlist.append("   <HRBefore>0</HRBefore>")
+        XMLlist.append("   <HRExtreme>0</HRExtreme>")
+        XMLlist.append("   <O2Before>0</O2Before>")
+        XMLlist.append("   <O2Min>0</O2Min>")
+        XMLlist.append("</Event>")
     #print(XMLlist)
 
-    #save at output
-with open(folderpath+"outputSleepStage.txt", 'w') as f:
-    for item in XMLlist:
-        f.write("%s\n" % item)
+    # save at output
+    with open(folderpath + "outputRespEvents.txt", 'w') as f:
+        for item in XMLlist:
+            f.write("%s\n" % item)
